1395280b2bea5dc1567a411787c3c0a158800c76
Sergio González Díaz <sergio.g.d7@gmail.com>
2025-07-16 13:25:28 +0200

    Add files via upload

---
+++ ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  TicTacToe
+//
+//  Created by Sergio González Díaz on 4/7/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        TicTacToeView()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
